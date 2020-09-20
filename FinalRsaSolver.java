f0aef45f26e602e38d696067101d9c7fa6b663e3
Piratica Vitae <biplavgautam1@gmail.com>
2020-09-20 19:03:14 -0400

    Created simple tool to solve RSA challenges.

---
+++ FinalRsaSolver.java
@@ -0,0 +1,159 @@
+import java.util.*;
+
+public class FinalRsaSolver {
+    public static void main(String[] args){
+
+        System.out.println("00000000000000KKKK000000KKKKK000000000000OOOOO00OOOOOOOO00000000000OOOOOkkkkkkkkkkxdooddddxxdooodddx");
+        System.out.println("000000000KKK00KKK0000000000K00000000000000OOO0000OOO00000000000000000OOOOOOOO00000OOkkkOOOkkkkxxxxkk");
+        System.out.println("00K0xkO0KKKKKKKKK0000000000000OOO000OOOOOOOOO00000O0000OOOOO0000000000OO000000000OOOOO000000000000OO");
+        System.out.println("000x'..,:ldk0KK00000000000000OkkO000OOOOOOOkkkOOOOOOO00OOOO0000O00000000000KKKK00OOkkO00KKK0OxxO000O");
+        System.out.println("000Ol.     .':oxk0000K000OOO000000000000000OOO0000000000000KKKK0000000000KKKKKK000OO00Oxoc;'..,kK00O");
+        System.out.println("O0000d;....    ..,lxOKKKK000KKK0K00K0000KKK0KKKKKKKKKKK00KKXXXKK00KKK00KKKKKKK000Oxl:,..   ..'d0K00O");
+        System.out.println("O0000kl,......     .':d0KKKKK000000KKKKKKK00KKKKKKKKKK0000KKKK0000KKKK00000KKOdc,..  ...'''';oOK0000");
+        System.out.println("0000000Ol,''.....      'ok00000000000000KKK000KKKKXXKKKKKKKK0000000000000Okd:.     ...'''..:kKK00000");
+        System.out.println("00KK000Kx;..',,'.....';coxk0K000KK000000000OOO000000000KKXKK00KKKKK00KKX0x:.   .....''',:cok0000OO00");
+        System.out.println("0K0000K00kc..''....,lxOKK000XNXXKKK00000OOOO00Oxdoodddk0KXKKKKKXXXKKKXXKxc....''.......':x00OOOOOOOO");
+        System.out.println("000000K00Ox:''',,,,cxOKXXKXKdodlldO0OO0000000xc;;;;;;;;l0KK000KKKO0XNX000xc,''''''''',:cdOK0OO0000OO");
+        System.out.println("00000000OO0Ol..'''';oxxdoccc'    .':dO0KK000xc;::;;,'..oKKK000Od:';:cxKXXKkc'........,d0KKKK00KKKKK0");
+        System.out.println("00000000OOOOxc,,,''''''... .'..   . .:k0K00kl:;,,,,',',xXKKK0o,..   'kXXXKOx:......':dO000K0O0O0000O");
+        System.out.println("0000KKK00000KOl'...''..... .,..  .    'dK0Oo::;,''','.;OKKXk;. .   .',:dO0kl,......,kK000KK000OO0000");
+        System.out.println("00000KKKK0000Oo;,''........ .'.   .   .'dKkc:;,''',,;,l000d'  ..  ......':do,....'cxO00KKK0000000000");
+        System.out.println("0000000000K0O00d,.'',,.........         'oo:,,,,;;;;::oOk:..  .  .''....'...',,,':k0OkO00OOOO000O00O");
+        System.out.println("OO000KK00OOO00Okc;;'...''.. ....   ..  ..,c;,,,,;;;;::cc,.. ..   ''..'....''..,cdkOOkkxkOOOOOO00000O");
+        System.out.println("00000000OOOOOOOOOx;.',,...... ... .   ...':;,'..'''',;;'.. ... .......,;'..',''o0OOOkkkO000000KKK000");
+        System.out.println("00OOkO000OOOOOO00Oo:,'..','. ......   ...';,'.......';;'....',......,'.';;'''ck0KKK0OO000O00KKK00K00");
+        System.out.println("00OO0KK00K00OO0KKOOk:..',..... ...   ....';,'.....'';c:'.'..,;;'....';,',::;,oO0KKK000000O00KXK00000");
+        System.out.println("000000K0000000000000ko:,........    ......',,'...',,;;;,'...,:,''.',..,'..ckOOOO0000KKK0000KKK0000KK");
+        System.out.println("K00000OO00OkkO00000KKKx,........    .......',,,;,,,''''......,,''......,;;o000OOkO0KKK0OO000000O000O");
+        System.out.println("O000OOkxk0OkkOOO0OO00K0Oo'....   .;;.......',;:;;;;'............... .. .l000OOO00O0K0OOOO000000OO00O");
+        System.out.println("OO00OkxxkOOkkkkkOOOOOOkOOoc;...,lkOd:;:;..'';::;:::,'......;, ... .. .,lk0OkOkO0K0000000000OOOKK0000");
+        System.out.println("OO0OkkOOOOkkkkOOO00OkkOO00ko:cxOOOOOkkOc,::;::::;;;,,,'.,clxk:.... '::okO0OkOO0OkOOOOOkOOOO00O0KK0O0");
+        System.out.println("0000OO00OO0OkO00O0000O000OOOO0OkOOkOkkklcc::::cc:;,,,;;'cOOkOOo,.';,dK0kkOOkO00O0K0OkkkkOOkkOOO00OO0");
+        System.out.println("0K0OOOOOOOOOOOOOkkO0OO000OOOO00OOOkOOOkdol:::cclc;;;;,,,ckOkO00OolkkxO0OOkxdxOO0KKK00OkkOO000000KK00");
+        System.out.println("00KOkO000O0K0OkkkO0OO000000OO0OOO0O0000kocc:::ccc;;;;,'.;k000OO0K00OkO000O000K0O000O0K0000K000OO0K0O");
+        System.out.println("OO0OxxkOOO0000OO00OOOOO00000OOOO0000OOkkoccc:::::;;;:;,.ckOOkxkOOkkOOO00O000000000O00000000OOOOO00OO");
+        System.out.println("OOOO0000OO0KKK0OOOOOOkkkkOOkkxxOOkkkxxxkdlc:;;:;;;;;;;,:x0OOkxxxxkkOOkkxkOOkkOO00kOOO00OOOkxxxxxxddx");
+        System.out.println("O0000K0000K0000O00OkOOOOOOOOOxxOOxkOkxxxxdc:,,;,;;,;;'.cO0OkOOOO00KK000O00K00O0K000O00K00OkOkkkkOOOO");
+        System.out.println("O0000OOOO0K00000KKOO00K00K0OOOOOOOOOOOOOOxl;;,.....':'.l00OOOOO0OOO0OO00000O0000K00000KKOO00kxk0K000");
+        System.out.println("O00OOOOkk000OOO0KK0OkO00000OkkOOOOOOOOOkkkc,;.. ...'c,'x0000000KXK0KKK00K0OO000K0000K0KKOk00Oxk00000");
+        System.out.println("OOkO00OOOOOO0O0OO00kkO00000OOO0OkkOOOOOkk0o'..  ....;',kK000000KX000KKK00OxkOOOOkOOkk0K0000Odlk000OO");
+        System.out.println("00OOO0Okxk000K0OO000OO00K00OO0000OOOOO0000d,..  .. .::lKKKK0000000OO0K0OOOkddxkkOOOkkO00KKOOOOOOOOkO");
+        System.out.println("0OOkkOOOkk0K0O000OOOOO00OO0OOO0K0OOOOOOOOOd,..  ....;:lOkOOkOO0K0OO0KKK0OxxxooxO00OOOOO0K0k0K0OOOkOO");
+        System.out.println("OkO0K0OOkkkOkk0000O00OO0000OO00OOkkOOO0KK0d..    ...,:lOOOOdox00OOOKKKOkOO00000KK0O00O0000OOKK000000");
+        System.out.println("OOOO00OO00OOO000OOOxxxxxxxkOkO000OO0OOOO00o.        .;o0OOkdxk000O00KKOO0OO0K0Oxk000000000OO0O00kO00");
+        System.out.println("0OOOO0OO0000OOOOOdlcc:;;',;:::ccccllccclol,         ..:kOkkkxdddk00000OkxkOk00Oxk00000OOOkkOOO00kkOO");
+        System.out.println("0OOO0OkO00OO00OkkOOkxoolllc:cll:;,'.''.';;. .    ......,,''',''.,cl:;;;,',;;:ccclcccc:;''',clloddood");
+        System.out.println("00KKOddkOOOkOOO00O00kxkOO00OOKKOkkxxkxox0d,:l. ..........,:ldxoldxdoddoccllcc::cc:::;,,;;,,,;;;loccl");
+        System.out.println("OKK000OOO00OkkO000OOOOOkk00kO00000OOOkkO0OO0d' .......,lkOkxxkOkOO000K00KKOxkO0000OOOOkkkkxolccllllo");
+        System.out.println("0000KK0000000OO000OOOkOkkOO00OOO0OOOOOOO0Okxxl::cllcccdkO0OO00O0000OO0KK000OO0K000000OOO00OOkkxxddxk");
+        System.out.println("K0kk00O00OO00K0OO0000000K0kxOkkkO0KK0000K0O0KOk000K0O00000OO00O0K0O00KKK0OkOOOOOxxkkOOO0OOOOkxxkOkkx");
+        System.out.println("0OkO00OOO0Ok0K00K00000O0000OOkkkkOOkxkkO000KKOO0K00OkO0000kkOOO0000K0O0OO0OOOOkkkOOkxxkkOOk0K0Okxddd");
+        System.out.println("xOOOOO0K00Ok00O000K0OOOOO00O0OOOkxxdoxkkO0KK0000000O0K0000OO00OO00OOkk00KK0OOOkO00OkkkOkkOkkOOkkkkxx");
+        System.out.println("O0OOOOOOOkx0000OO00KK00OkO00OOkOO00000OkOO0KOOK0OkxkOOOO0KOkO0KK0OOkOOO0OOOOkkkxddkkkOkO0OxxOkOOkkOO");
+        System.out.println();
+        System.out.println();
+
+        //Prime factor calculation
+        int n, p, q, ncopy;
+        Integer primes[] = {};
+        final  Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        n = keyboard.nextInt();
+        ncopy = n;
+        List<Integer> primesList = new ArrayList<Integer>(Arrays.asList(primes));
+
+        System.out.print("Prime factors are: ");
+        for(int i=2; i<=ncopy; i++){
+            while(ncopy%i == 0){
+                System.out.print(i + "  ");
+                primesList.add(i);
+                primes = primesList.toArray(primes);
+                ncopy = ncopy/i;
+            }
+        }
+
+        System.out.println();
+        //Phi(n) and decryption key(d) calculation
+        //p & q are primes, and e is given public key
+        int e, phin, d=0;
+        p = primes[1];
+        q = primes[0];
+        System.out.println("p is " + p + " and q is " + q); 
+        System.out.println();
+
+        phin = (p-1)*(q-1);
+
+        System.out.println("Enter the value of e: ");
+        e = keyboard.nextInt();
+
+        //Here, i acts as an constant(k) for math calculation, which we keep on increasing by 1, until we get make it perfectly divisible
+        for(int i=0; i<=e; i++){
+
+            if((1+(i*phin))%e == 0){
+                System.out.println("D can be calculated by substituting i with "+ i);
+                System.out.println();
+                System.out.println("Calculating d. . . . . . . ");
+                System.out.println();
+                d = (1+(i*phin))/e;
+                System.out.println("The value of d is: " + d);
+                System.out.println();
+            }
+
+        }
+        
+        //Decryption
+        //////////////////////////////////////////////////////////// 
+        ////////////////////////////////////////////////////////////
+        //Comes in as a string 
+        //Needs to be a number and iterate through
+
+        System.out.println("Enter cipher: ");
+        final Scanner keyboard1 = new Scanner(System.in);
+        final String cipher = keyboard1.nextLine();
+        
+        splitStringToArray(cipher, d, n);
+        keyboard1.close();
+        keyboard.close();
+    }
+
+    //Split string method
+    public static void splitStringToArray(String keyboard, int d, int n){
+
+        //Holds the split string flag 
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();  
+
+        //Split the string
+        //add split string to above arraylist
+
+        String[] spl = keyboard.split(" ");
+        for(int i = 0; i < spl.length; i++){
+            listOfNumbers.add(Integer.parseInt(spl[i]));
+
+        }
+        
+        System.out.println();
+        System.out.print("Flag Found: \n");
+        //New array holding the math number below
+        ArrayList<Long> plainTextz = new ArrayList<>();
+        for(int i = 0; i < listOfNumbers.size(); i++){
+
+            //Math
+
+            long b = 1;
+            for (int j=0; j<d; j++){
+                b = (b*listOfNumbers.get(i)) %n;
+            }
+            plainTextz.add(b);
+
+            //print array
+            int intValue = plainTextz.get(i).intValue();
+            char solved = (char)intValue;
+            System.out.print(solved);
+
+        }
+
+        System.out.println();
+    }
+    
+}
+
